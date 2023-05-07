48bebf686d231a5dc8e6df676671047c7a6aae11
Duncanbj01 <83521298+Duncanbj01@users.noreply.github.com>
2023-05-07 18:56:55 -0500

    Arduino Game Controller

--- MAGD 372 Assignments Project/Assets/Scripts/Move.cs
+++ MAGD 372 Assignments Project/Assets/Scripts/Move.cs
@@ -1,34 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO.Ports;
+using System.IO.Ports; //Contains classes for controlling serial ports.
 
 
 public class Move : MonoBehaviour
 {
-    //Unity connects to serial port 
+    //Unity connects to the serial port that the Arduino is using which in this case is "COM5".  
     SerialPort sp = new SerialPort("COM5", 9600);
-   // public string recievedString; 
 
    
     void Start()
     {
-        sp.Open();
-        sp.ReadTimeout = 100;  
-        
+        sp.Open();. //Opens the serial port connection. 
+        sp.ReadTimeout = 100;  //Sets the number of milliseconds before a timeout occurs when a read operation doesnt finish. 
+                               //This happens if the server takes too long to responds and send information. 
+
     }
 
     void Update()
     {
-        if (sp.IsOpen)
+        if (sp.IsOpen) //tracks whether the port is open for use by the caller which in this case is Unity
         {
-            try
+            try // the try statment allows you to test a block of code (button inputs) for errors while the code is being executed
             {
-                
-                if (sp.ReadByte() == 1)
+                if (sp.ReadByte() == 1) //Synchronously reads a byte from the serial port. //Communicates with Unity and recognizes input as pin_button1 which allows the game object to move up. 
                 {
-                    transform.Translate(Vector3.up * Time.deltaTime * 4);
-                    Debug.Log("Up"); 
+                    transform.Translate(Vector3.up * Time.deltaTime * 4); //C# code that allows the game object to move in a direction depending on what button is pressed. 
+                                                                          //Time.deltaTime * 4 controls the speed of the game object. 
+
+                    Debug.Log("Up"); //prints the input to the console inside of unity. 
                 }
                 if (sp.ReadByte() == 2)
                 {
@@ -67,7 +68,8 @@ public class Move : MonoBehaviour
                     Debug.Log("Circle");
                 }
             }
-            catch (System.Exception)
+            catch (System.Exception) // if an error occurs (button input doesnt work) either the system or application(Unity) reports it to the user
+                                     // by throwing an exception containing info about the error. 
             {
 
             }
